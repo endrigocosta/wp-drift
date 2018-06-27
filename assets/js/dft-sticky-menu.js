588a83f985820a7de6a58ffdf4fbef2145cdc02a
Willon <willonnava@gmail.com>
2018-06-26 23:09:45 -0300

    criacao do arquivo js do sticky menu

---
+++ assets/js/dft-sticky-menu.js
@@ -0,0 +1,10 @@
+/**
+ * Sticky Menu
+ * Faz o trabalho de adicionar uma classe no nav
+ * em determinada distância do topo do site, utilize
+ * CSS para animações do mesmo.
+ */
+
+ jQuery(function($) {
+    // Em construção
+ });
